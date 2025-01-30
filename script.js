cbdd11540abb5277c51e5381a05180de357fd572
poly1997p <poly20221997@gmail.com>
2025-01-30 21:47:48 +0600

    js variable

---
+++ script.js
@@ -0,0 +1,72 @@
+//General way
+
+x= 40;
+y= 50;
+
+result = x+y;  //90
+
+console.log(result);
+
+//using var
+
+var x= 40;
+var y= 60;
+
+var result = x+y;
+
+console.log(result);
+
+//using let
+
+
+let m= 200;
+let n= 400;
+let value = m+n;
+
+console.log(value);
+
+// using const
+
+
+const o= 500;
+const p= 400;
+const manu = o+p;
+
+console.log(manu);
+
+//subtraction
+
+let s = 100;
+let q = 80;
+
+let sub = s-q;
+
+console.log(sub)
+
+//multiplication
+
+
+let digit1 = 100;
+let digit2 = 2;
+
+let mul = digit1*digit2;
+
+console.log(mul)
+
+//divition
+
+let data1 = 67;
+let data2 = 5;
+
+let div = data1/data2;
+
+console.log(div);
+
+//remainder
+
+let data3 = 5;
+let data4 = 6;
+
+let remainder = data3%data4;
+
+console.log(remainder)
